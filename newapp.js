c61d60ce5faa901ba9f32d20e9656a3e457b4574
SPC David Wiliams II <davidwilliams1102@yahoo.com>
2019-09-23 14:54:26 -0500

    started to work on main menu to display family descendants restart or quit the app

--- newapp.js
+++ newapp.js
@@ -58,27 +58,24 @@ function mainMenu(person, people, searchResults, foundPerson, personInfo){
     return app(people); // restart
   }
 
-  let displayOption = prompt("Found " + person.firstName + " " + person.lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'");
+  let displayOption = prompt("Found " + person.firstName + " " + person.lastName + " . Do you want to know their  'family', or 'descendants'? Type the option you want or 'restart' or 'quit'");
 
-//   switch(displayOption){
-//     case "info":
-
-//                       // TODO: get person's info
-//     break;
-//     case "family":
-//                        // TODO: get person's family
-//     break;
-//     case "descendants":
-//                          // TODO: get person's descendants
-//     break;
-//     case "restart":
-//     app(people); // restart
-//     break;
-//     case "quit":
-//     return; // stop execution
-//     default:
-//     return mainMenu(person, people, searchResults); // ask again
-//   }
+   switch(displayOption){
+    break;
+    case "family":
+     displayFamily                  // TODO: get person's family
+    break;
+    case "descendants":
+                         // TODO: get person's descendants
+    break;
+    case "restart":
+    app(people); // restart
+    break;
+    case "quit":
+    return; // stop execution
+    default:
+    return mainMenu(person, people, searchResults); // ask again
+  }
 }
 
 function searchByName(people){
